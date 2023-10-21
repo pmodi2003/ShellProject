3de3a396496ddc3f2f674c4305920612f0a018f6
Priyam Modi <modi.priyam@northeastern.edu>
2023-10-21 20:04:55 +0000

    tokenizer function

--- tokenize.c
+++ tokenize.c
@@ -1,4 +1,26 @@
 
+char **tokenize(const char *input){
+  char **tokens = NULL;
+  int token_count = 0;
+
+  char *token = strtok(input, " \t\n");
+
+  while(token != NULL){
+    token_count++;
+    tokens = realloc(tokens, token_count * sizeof(char*));
+    tokens[token_count-1] = strdup(token);
+    token = strtok(NULL, " \t\n");
+  }
+
+  tokens = realloc(tokens, (token_count+1) * sizeof(char*));
+  tokens[token_count] = NULL;
+
+  return tokens;
+
+}
+
 int main(int argc, char **argv) {
-  // TODO: Implement the tokenize demo.
+  char input[255];
+  printf("Enter a string: ");
+
 }
